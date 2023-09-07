e6e25a4ae4162003f784dc354a27fd078bb98527
JBennett22 <79475895+JBennett22@users.noreply.github.com>
2023-09-06 20:31:50 -0400

    Implementing tabs

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
 //Root component (App.js) that is entry point for application
 import React from "react"
-import { View, StyleSheet } from "react-native"
 import CurrentWeather from "./src/screens/CurrentWeather"
 import UpcomingWeather from "./src/screens/UpcomingWeather"
 import City from "./src/screens/City"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+const Tab = createBottomTabNavigator ()
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-      {/* <UpcomingWeather /> */}
-      {/* <City /> */}
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CurrentWeather} />
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
+        <Tab.Screen name={"City"} component={City} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 
 export default App
