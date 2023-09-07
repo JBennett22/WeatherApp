d14c6bc0e0eb16a23707a92c8f6ab40fc9c740b0
JBennett22 <79475895+JBennett22@users.noreply.github.com>
2023-09-07 17:57:28 -0400

    Add weather types

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -2,6 +2,7 @@ import React from "react"
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons';
 import RowText from "../components/RowText";
+import { weatherType } from "../utilities/weatherType";
 
 //CurrentWeather component. Returns some JSX code that is turned into javascript under the hood by babel. It is then exported and rendered to the user.
 
@@ -22,7 +23,7 @@ const CurrentWeather = () => {
       </View>
       <RowText 
         messageOne={'Its sunny'} 
-        messageTwo={'Its perfect t-shirt weather'} 
+        messageTwo={weatherType['Thunderstorm'].message} 
         containerStyles={styles.bodyWrapper} 
         messageOneStyles={styles.description} 
         messageTwoStyles={styles.message} 
