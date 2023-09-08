e6051371576b65619c289fe040c374c7fdce9238
JBennett22 <79475895+JBennett22@users.noreply.github.com>
2023-09-08 18:58:13 -0400

    Add useGetWeather component
    Migrate the fetching of weather data to its own component

--- App.js
+++ App.js
@@ -5,45 +5,12 @@ import { NavigationContainer } from "@react-navigation/native"
 import Tabs from "./src/components/Tabs"
 import * as Location from 'expo-location'
 import { WEATHER_API_KEY } from '@env';
+import { useGetWeather } from "./src/hooks/useGetWeather"
 
 const App = () => {
-  // Set initial loading state
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
+  const [loading, error, weather] = useGetWeather()
 
-  // Fetch the weather based on location from the API
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-      const data = await response.json()
-      setWeather(data)
-    } catch (error) {
-      setError('Could not fetch weather')
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    ;(async() => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        setError('permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if (weather) {
-    console.log(weather)
-  }
+  console.log(loading, error, weather)
 
   if (loading) {
     return (
